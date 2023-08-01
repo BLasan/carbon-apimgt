cfca5c2518200352c415855524d7fd466e3e6a04
dushani <dushaniw@wso2.com>
2023-08-01 17:38:08 +0530

    add test case.

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/test/java/org/wso2/carbon/apimgt/impl/definitions/OAS2ParserTest.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/test/java/org/wso2/carbon/apimgt/impl/definitions/OAS2ParserTest.java
@@ -19,24 +19,30 @@
 
 package org.wso2.carbon.apimgt.impl.definitions;
 
-import io.swagger.models.HttpMethod;
-import io.swagger.models.Operation;
-import io.swagger.models.Path;
-import io.swagger.models.Swagger;
-import io.swagger.models.auth.OAuth2Definition;
+import io.swagger.models.*;
+import io.swagger.models.auth.*;
 import io.swagger.parser.SwaggerParser;
 import org.apache.commons.io.IOUtils;
+import org.apache.synapse.unittest.testcase.data.classes.AssertNotNull;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.wso2.carbon.apimgt.api.APIDefinitionValidationResponse;
 import org.wso2.carbon.apimgt.api.ExceptionCodes;
+import org.wso2.carbon.apimgt.api.model.API;
+import org.wso2.carbon.apimgt.api.model.APIIdentifier;
+import org.wso2.carbon.apimgt.api.model.Scope;
 import org.wso2.carbon.apimgt.api.model.URITemplate;
 import org.wso2.carbon.apimgt.impl.APIConstants;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -156,5 +162,145 @@ public class OAS2ParserTest extends OASTestBase {
                 response.getErrorItems().get(0).getErrorCode());
         Assert.assertEquals(ExceptionCodes.INVALID_OAS2_FOUND.getErrorCode(),
                 response.getErrorItems().get(1).getErrorCode());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testGetOASDefinitionForStore() throws Exception {
+        // Testing API with all 3 oauth, basic and api key security with scopes.
+        String swagger = IOUtils.toString(
+                getClass().getClassLoader().getResourceAsStream("definitions" + File.separator + "oas2"
+                        + File.separator + "oas2AllSecurity.json"), String.valueOf(StandardCharsets.UTF_8));
+        APIIdentifier apiIdentifier = new APIIdentifier("admin", "SwaggerPetstore", "1.0.0");
+        Map<String, String> hostWithSchemes = new HashMap<>();
+        hostWithSchemes.put(APIConstants.HTTPS_PROTOCOL, "https://localhost");
+        API api = new API(apiIdentifier);
+        api.setApiSecurity("oauth_basic_auth_api_key_mandatory,api_key,basic_auth,oauth2");
+        api.setTransports("https");
+        api.setContext("/petstore");
+        api.setScopes(getAPITestScopes());
+        String swaggerForStore = oas2Parser.getOASDefinitionForStore(api, swagger, hostWithSchemes);
+        SwaggerParser swaggerParser = new SwaggerParser();
+        Swagger swaggerObjForStore = swaggerParser.parse(swaggerForStore);
+        Assert.assertNotNull(swaggerObjForStore.getSecurityDefinitions());
+        Map<String, SecuritySchemeDefinition> securitySchemeDefinitionMap = swaggerObjForStore.getSecurityDefinitions();
+        // Test basic auth security definition.
+        Assert.assertNotNull(securitySchemeDefinitionMap.get(APIConstants.API_SECURITY_BASIC_AUTH));
+        BasicAuthDefinition basicAuthDefinition = (BasicAuthDefinition)
+                securitySchemeDefinitionMap.get(APIConstants.API_SECURITY_BASIC_AUTH);
+        Assert.assertTrue(basicAuthDefinition.getVendorExtensions()
+                .containsKey(APIConstants.SWAGGER_X_BASIC_AUTH_SCOPES));
+        Assert.assertTrue(basicAuthDefinition.getVendorExtensions()
+                .containsKey(APIConstants.SWAGGER_X_SCOPES_BINDINGS));
+        testAPIScopes((Map<String, String>) basicAuthDefinition.getVendorExtensions()
+                        .get(APIConstants.SWAGGER_X_BASIC_AUTH_SCOPES),
+                (Map<String, String>) basicAuthDefinition.getVendorExtensions()
+                        .get(APIConstants.SWAGGER_X_SCOPES_BINDINGS));
+        // Test OAuth security definition.
+        Assert.assertNotNull(securitySchemeDefinitionMap.get(APIConstants.SWAGGER_APIM_DEFAULT_SECURITY));
+        Assert.assertEquals(securitySchemeDefinitionMap.get(APIConstants.SWAGGER_APIM_DEFAULT_SECURITY).getType(),
+                APIConstants.DEFAULT_API_SECURITY_OAUTH2);
+        Assert.assertTrue(securitySchemeDefinitionMap.get(APIConstants.SWAGGER_APIM_DEFAULT_SECURITY)
+                .getVendorExtensions().containsKey(APIConstants.SWAGGER_X_SCOPES_BINDINGS));
+        OAuth2Definition oAuth2Definition = ((OAuth2Definition) securitySchemeDefinitionMap
+                .get(APIConstants.SWAGGER_APIM_DEFAULT_SECURITY));
+        testAPIScopes(oAuth2Definition.getScopes(), (Map<String, String>) oAuth2Definition.getVendorExtensions()
+                .get(APIConstants.SWAGGER_X_SCOPES_BINDINGS));
+        // Test API Key security definition.
+        Assert.assertNotNull(securitySchemeDefinitionMap.get(APIConstants.API_SECURITY_API_KEY));
+        ApiKeyAuthDefinition apiKeyAuthDefinition = (ApiKeyAuthDefinition) securitySchemeDefinitionMap
+                .get(APIConstants.API_SECURITY_API_KEY);
+        Assert.assertEquals(apiKeyAuthDefinition.getIn(), In.HEADER);
+        Assert.assertEquals(apiKeyAuthDefinition.getName(), APIConstants.API_KEY_AUTH_TYPE);
+        // Test Swagger Security.
+        List<SecurityRequirement> securityRequirements = swaggerObjForStore.getSecurity();
+        Assert.assertNotNull(securityRequirements);
+        Assert.assertEquals(securityRequirements.size(), 3);
+        SecurityRequirement defaultSec = new SecurityRequirement();
+        defaultSec.setRequirements(APIConstants.SWAGGER_APIM_DEFAULT_SECURITY, new ArrayList<>());
+        SecurityRequirement basicSec = new SecurityRequirement();
+        basicSec.setRequirements(APIConstants.API_SECURITY_BASIC_AUTH, new ArrayList<>());
+        SecurityRequirement apiKeySec = new SecurityRequirement();
+        apiKeySec.setRequirements(APIConstants.API_SECURITY_API_KEY, new ArrayList<>());
+        Assert.assertArrayEquals(securityRequirements.toArray(),
+                new SecurityRequirement[]{defaultSec, basicSec, apiKeySec});
+        // Test operation level security.
+        for (Map.Entry<String, Path> pathEntry : swaggerObjForStore.getPaths().entrySet()) {
+            for (Operation operation : pathEntry.getValue().getOperations()) {
+                List<Map<String, List<String>>> opSecurity = operation.getSecurity();
+                Assert.assertFalse(opSecurity.isEmpty());
+                if ("post".equals(operation.getOperationId())) {
+                    Map<String, List<String>> defaultOpSec = new HashMap<>();
+                    defaultOpSec.put(APIConstants.SWAGGER_APIM_DEFAULT_SECURITY, new ArrayList<String>() {{
+                        add("PetLocalScope");
+                        add("GlobalScope");
+                    }});
+                    Map<String, List<String>> basicOpSec = new HashMap<>();
+                    basicOpSec.put(APIConstants.API_SECURITY_BASIC_AUTH, new ArrayList<>());
+                    Map<String, List<String>> apiKeyOpSec = new HashMap<>();
+                    apiKeyOpSec.put(APIConstants.API_SECURITY_API_KEY, new ArrayList<>());
+                    List<Map<String, List<String>>> expectedSec = new ArrayList<Map<String, List<String>>>() {{
+                        add(defaultOpSec);
+                        add(basicOpSec);
+                        add(apiKeyOpSec);
+                    }};
+                    Assert.assertArrayEquals(opSecurity.toArray(), expectedSec.toArray());
+                    Assert.assertTrue(operation.getVendorExtensions()
+                            .containsKey(APIConstants.SWAGGER_X_BASIC_AUTH_RESOURCE_SCOPES));
+                    List<String> basicOpScopes = (ArrayList<String>) operation.getVendorExtensions()
+                            .get(APIConstants.SWAGGER_X_BASIC_AUTH_RESOURCE_SCOPES);
+                    Assert.assertArrayEquals(basicOpScopes.toArray(), new String[]{"PetLocalScope", "GlobalScope"});
+                }
+                if ("put".equals(operation.getOperationId())) {
+                    Map<String, List<String>> defaultOpSec = new HashMap<>();
+                    defaultOpSec.put(APIConstants.SWAGGER_APIM_DEFAULT_SECURITY, new ArrayList<String>() {{
+                        add("PetLocalScope");
+                    }});
+                    Map<String, List<String>> basicOpSec = new HashMap<>();
+                    basicOpSec.put(APIConstants.API_SECURITY_BASIC_AUTH, new ArrayList<>());
+                    Map<String, List<String>> apiKeyOpSec = new HashMap<>();
+                    apiKeyOpSec.put(APIConstants.API_SECURITY_API_KEY, new ArrayList<>());
+                    List<Map<String, List<String>>> expectedSec = new ArrayList<Map<String, List<String>>>() {{
+                        add(defaultOpSec);
+                        add(basicOpSec);
+                        add(apiKeyOpSec);
+                    }};
+                    Assert.assertArrayEquals(opSecurity.toArray(), expectedSec.toArray());
+                    Assert.assertTrue(operation.getVendorExtensions()
+                            .containsKey(APIConstants.SWAGGER_X_BASIC_AUTH_RESOURCE_SCOPES));
+                    List<String> basicOpScopes = (ArrayList<String>) operation.getVendorExtensions()
+                            .get(APIConstants.SWAGGER_X_BASIC_AUTH_RESOURCE_SCOPES);
+                    Assert.assertArrayEquals(basicOpScopes.toArray(), new String[]{"PetLocalScope"});
+                }
+            }
+        }
+    }
+
+    private Set<Scope> getAPITestScopes() {
+        Scope petLocalScope = new Scope();
+        petLocalScope.setKey("PetLocalScope");
+        petLocalScope.setName("PetLocalScope");
+        petLocalScope.setRoles("admin");
+        petLocalScope.setDescription("admin");
+        Scope globalScope = new Scope();
+        globalScope.setName("GlobalScope");
+        globalScope.setKey("GlobalScope");
+        globalScope.setDescription("desc");
+        globalScope.setRoles("");
+        Set<Scope> apiScopes = new HashSet<>();
+        apiScopes.add(globalScope);
+        apiScopes.add(petLocalScope);
+        return apiScopes;
+    }
+
+    private void testAPIScopes(Map<String, String> swaggerScopes, Map<String, String> swaggerScopeToRoleBindings) {
+        Assert.assertTrue(swaggerScopes.containsKey("PetLocalScope"));
+        Assert.assertEquals(swaggerScopes.get("PetLocalScope"), "admin");
+        Assert.assertTrue(swaggerScopes.containsKey("GlobalScope"));
+        Assert.assertEquals(swaggerScopes.get("GlobalScope"), "desc");
+        Assert.assertTrue(swaggerScopeToRoleBindings.containsKey("PetLocalScope"));
+        Assert.assertEquals(swaggerScopeToRoleBindings.get("PetLocalScope"), "admin");
+        Assert.assertTrue(swaggerScopeToRoleBindings.containsKey("GlobalScope"));
+        Assert.assertEquals(swaggerScopeToRoleBindings.get("GlobalScope"), "");
     }
 }
