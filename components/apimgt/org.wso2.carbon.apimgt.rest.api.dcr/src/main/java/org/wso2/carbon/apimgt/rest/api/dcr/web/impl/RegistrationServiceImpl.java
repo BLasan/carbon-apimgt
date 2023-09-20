a85d2db33f80b2a6e34292fa025a7f7c8535f2bd
Prasanna Dangalla <prasannadangalla@gmail.com>
2023-09-20 11:40:40 +0530

    Revert "Fixed DCR admin issue."
    This reverts commit 3198e852dd0b2e38753695107b9a5199be578eae.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.dcr/src/main/java/org/wso2/carbon/apimgt/rest/api/dcr/web/impl/RegistrationServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.dcr/src/main/java/org/wso2/carbon/apimgt/rest/api/dcr/web/impl/RegistrationServiceImpl.java
@@ -273,7 +273,8 @@ public class RegistrationServiceImpl implements RegistrationService {
             String appOwner = consumerAppDTO.getUsername();
 
             if (MultitenantConstants.SUPER_TENANT_DOMAIN_NAME.equals(
-                    MultitenantUtils.getTenantDomain(consumerAppDTO.getUsername()))) {
+                    MultitenantUtils.getTenantDomain(consumerAppDTO.getUsername()))
+            ) {
                 appOwner = applicationOwner;
             }
 
