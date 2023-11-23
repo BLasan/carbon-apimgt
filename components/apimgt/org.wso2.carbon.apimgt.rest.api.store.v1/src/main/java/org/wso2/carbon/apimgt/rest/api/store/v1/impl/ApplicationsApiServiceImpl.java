8f2b5582b7d7a1bb9d9b3f9b74f4312fba1e893d
GihanAyesh <gihanayeshmanthadrck@gmail.com>
2023-11-23 12:58:24 +0530

    Check for application sharing access

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/ApplicationsApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/ApplicationsApiServiceImpl.java
@@ -635,7 +635,8 @@ public class ApplicationsApiServiceImpl implements ApplicationsApiService {
                                         new String(Base64.getUrlDecoder().decode(splitToken[1])));
                     org.json.JSONObject appInfo = decodedBody.getJSONObject(APIConstants.JwtTokenConstants.APPLICATION);
                     if (appInfo != null && application != null) {
-                        if (RestAPIStoreUtils.isUserOwnerOfApplication(application)) {
+                        String groupId = RestApiUtil.getLoggedInUserGroupId();
+                        if (RestAPIStoreUtils.isUserOwnerOfApplication(application) || application.getGroupId().equals(groupId)) {
                             String appUuid = appInfo.getString(APIConstants.JwtTokenConstants.APPLICATION_UUID);
                             if (applicationId.equals(appUuid)) {
                                 long expiryTime = Long.MAX_VALUE;
